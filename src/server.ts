146d2b5d2a07489fc21f38c6e0482ca5e8bc4dcd
faiithal <johndumlao2355@gmail.com>
2026-05-09 19:32:50 +0800

    added env schema validation and modified ts config

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ const app = express();
 
 app.get('/health', (req, res) => {
     res.status(200).json({
-        status: 'OK',
-        timestamp: new Date().toISOString,
-        service: 'Workout Tracker API',
+        "status": 'OK',
+        "timestamp": new Date().toISOString(),
+        "service": 'Workout Tracker API',
     })
 })
 
